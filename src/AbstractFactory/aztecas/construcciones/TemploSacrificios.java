102b769bddc035a533ce5bfd6c773577c737230b
Fredy <00082817@uca.edu.sv>
2018-06-05 10:47:38 -0600

    Se creó la clase TemploSacrificios en el paquete cosnstrucciones que hereda de Construccion y sobreescribe el método labor()

---
+++ src/AbstractFactory/aztecas/construcciones/TemploSacrificios.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory.aztecas.construcciones;
+
+
+/**
+ *
+ * @author fredy
+ */
+public class TemploSacrificios implements Construccion{
+    @Override
+    public void labor(){
+        System.out.println("Azteca sacrificado");
+    }
+}
