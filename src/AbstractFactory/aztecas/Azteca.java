3817ca8a9ba78aea40767a48177733ef227a2b5a
Fredy <00082817@uca.edu.sv>
2018-06-05 10:42:20 -0600

    Se creó la interfaz Azteca y se le creó el método get construccion

---
+++ src/AbstractFactory/aztecas/Azteca.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory.aztecas;
+
+import AbstractFactory.aztecas.construcciones.Construccion;
+
+/**
+ *
+ * @author fredy
+ */
+public interface Azteca {
+    Construccion getConstruccion(String type);
+}
