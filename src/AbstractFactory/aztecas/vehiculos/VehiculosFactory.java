128aa2985e6f425c0f84094aa0cb50747653d71b
Fredy <00082817@uca.edu.sv>
2018-06-07 17:59:00 -0600

    Se creó el paquete vehiculos en Aztecas y se creó la interfaz vehiculo Vehiculo y La Clase VehiculosFactory La cual hereda de Azteca

---
+++ src/AbstractFactory/aztecas/vehiculos/VehiculosFactory.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory.aztecas.vehiculos;
+
+import AbstractFactory.aztecas.Azteca;
+import AbstractFactory.aztecas.construcciones.Construccion;
+import AbstractFactory.aztecas.personajes.Personaje;
+
+/**
+ *
+ * @author fredy
+ */
+public class VehiculosFactory implements Azteca {
+
+    @Override
+    public Construccion getConstruccion(String type) {
+        return null;
+    }
+    
+    @Override
+    public Personaje getPersonaje(String type){
+        return null;
+    }
+    
+    @Override
+    public Vehiculo getVehiculo(String type){
+        return null;
+    }
+
+}
