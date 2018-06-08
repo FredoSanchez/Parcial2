e039ce75995f5f19bef5ba44ffec6ac70a2a43a9
Fredy <00082817@uca.edu.sv>
2018-06-08 17:53:22 -0600

    Se añadieron las clases GuerreroGuecha y GuerreroJaguar en el paquete personajes en aztecas que implementan de Personaje y sobreescriben sus métodos

---
+++ src/AbstractFactory/aztecas/personajes/GuerreroJaguar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory.aztecas.personajes;
+
+/**
+ *
+ * @author fredy
+ */
+public class GuerreroJaguar implements Personaje{
+    @Override
+    public void atacar(){
+        System.out.println("Guerrero Jaguar atacando");
+    }
+}
