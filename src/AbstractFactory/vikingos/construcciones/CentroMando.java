e30baed3e6bb9f33f357b5b5064e17bcec999001
Fredy <00082817@uca.edu.sv>
2018-06-09 13:39:01 -0600

    Godhaus

---
+++ src/AbstractFactory/vikingos/construcciones/CentroMando.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbstractFactory.vikingos.construcciones;
+
+/**
+ *
+ * @author fredy
+ */
+public class CentroMando implements Construccion{
+    @Override
+    public void labor(){
+        System.out.println("Centro de mando en construcción.");
+    }
+}
